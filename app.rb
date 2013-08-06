078a676036f935995e6b90641815e77073ba8835
Adina Beceriga <abeceriga@las.ch>
2013-08-06 10:02:22 +0200

    Added hello route with parameters

--- app.rb
+++ app.rb
@@ -9,3 +9,8 @@ end
 get '/hello' do
   "<h1>Hello world!</h1><br>This is the hello route<br>It's #{Time.now} at the server!"
 end
+
+get '/hello/:name' do
+  "<h1>Hello, #{params[:name]}!</h1><br>It's #{Time.now} at the hello - name route!"
+end
+  
