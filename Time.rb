2f93415b13e476d96d4922ae4c88daf1218e646d
Adina Beceriga <abeceriga@las.ch>
2013-09-18 10:14:20 +0200

    Initial Commit

--- Time.rb
+++ Time.rb
@@ -7,9 +7,9 @@ get '/' do
 end
 
 get '/1' do
-  "<h1>What is the current time?</h1>"
+  "<h0.2>What is the current time?</h0.2>"
 end
 
 get '/1/2' do
-  "<h10>What is the current time?</h10>"
+  "<h50>What is the current time?</h50>"
 end
